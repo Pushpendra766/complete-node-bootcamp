215b3493c3b9d3cb650b2f1d52516294373003af
Pushpendra766 <pushpendrachandravanshi4@gmail.com>
2022-07-17 09:03:53 +0530

    completed

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,7 +2,11 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-////////////////////////////
+const slugify = require("slugify");
+
+const replaceTemplate = require("./modules/replaceTemplate");
+
+///////////////////////////////
 ///// FILES ///////////////
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -27,21 +31,6 @@ const url = require("url");
 
 ////////////////////////////
 ///// SERVER ///////////////
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-    }
-    return output;
-};
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
@@ -58,6 +47,9 @@ const tempProduct = fs.readFileSync(
     `${__dirname}/templates/template-product.html`,
     "utf-8"
 );
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
